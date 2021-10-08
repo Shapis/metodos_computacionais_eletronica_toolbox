9b51e79e39786da2897fb14471666425bd3594b6
Henrique Silva <hpsilva+git@pm.me>
2021-10-08 04:45:43 -0300

    Fix typos

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='mcetoolbox',
-    version='0.0.2',
+    version='0.0.3',
     author='Henrique Silva',
     author_email='hpsilva@protonmail.com',
     description='Package com estruturas de dados da materia metodos computacionais para eletronica',
-    url='https://github.com/Shapis/mcetoolbox.git',
+    url='https://github.com/Shapis/metodos_computacionais_eletronica_toolbox.git',
     packages=['mcetoolbox'],
     license='GNU v3',
 )
