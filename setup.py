1fae33cadd647cdfba480848f9d403fb2e31094e
Henrique Silva <hpsilva+git@pm.me>
2021-10-08 04:34:01 -0300

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='metodos_computacionais_eletronica_toolbox',
-    version='0.0.1',
+    version='0.0.2',
     author='Henrique Silva',
     author_email='hpsilva@protonmail.com',
     description='Package com estruturas de dados da materia metodos computacionais para eletronica',
