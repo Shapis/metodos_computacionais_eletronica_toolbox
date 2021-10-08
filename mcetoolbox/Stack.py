7c98b32f2e644a21d169271538d2fec705b83623
Henrique Silva <hpsilva+git@pm.me>
2021-10-08 04:21:57 -0300

    Initial commit

---
+++ mcetoolbox/Stack.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self.len_stack = 0
+
+    def push(self, e):
+        self.stack.append(e)
+        self.len_stack += 1
+
+    def pop(self):
+        if self.len_stack != 0:
+            self.len_stack -= 1
+            return self.stack.pop()
+
+    def top(self):
+        if self.len_stack == 0:
+            return None
+        return self.stack[-1]
+
+    def show(self):
+        if self.len_stack == 0:
+            for i in self.stack:
+                print(i, end=' ')
+            print()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='metodos_computacionais_eletronica_toolbox',
+    version='0.0.1',
+    author='Henrique Silva',
+    author_email='hpsilva@protonmail.com',
+    description='Package com estruturas de dados da materia metodos computacionais para eletronica',
+    url='https://github.com/Shapis/mcetoolbox.git',
+    packages=['metodos_computacionais_eletronica_toolbox'],
+    license='GNU v3',
+)
